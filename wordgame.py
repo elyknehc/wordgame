75baaf06ab26495717ee9a5d5190c9b420f7de3d
elyknehc <kchen727@uwo.ca>
2025-02-11 12:14:26 -0500

    first milestone: randomly generate a word from a given dictoinary handle guessing the word correctly (True/false)

--- wordgame.py
+++ wordgame.py
@@ -1,1 +1,29 @@
-print("hello world")
+import random
+
+words = ["able", "belt", "bolt", "cast", "cash", "knot", "note", "near", "over", "salt", "wind"]
+
+class Word:
+
+    def __init__(self):
+        self.word = random.choice(words)
+
+class Game:
+
+    # Constructor accepts a word object
+    def __init__(self, word):
+        self.word = word
+        
+
+    def handleGuess(self, guess):
+
+        # Check if the guess is correct
+        if guess == self.word.word:
+            print("Correct!")
+        else:
+            print("Incorrect!")
+
+# Create a word object
+word = Word()
+game = Game(word)
+
+print(game.handleGuess("able"))
