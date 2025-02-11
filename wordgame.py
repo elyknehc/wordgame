a6ec928a78f35b3a6d408e01fd59380542fa099e
elyknehc <kchen727@uwo.ca>
2025-02-11 12:23:37 -0500

    handle user inputs for guessing
    determines when the user has won or lost with this new logic

--- wordgame.py
+++ wordgame.py
@@ -12,18 +12,33 @@ class Game:
     # Constructor accepts a word object
     def __init__(self, word):
         self.word = word
+        self.guesses = 5
         
 
     def handleGuess(self, guess):
 
         # Check if the guess is correct
         if guess == self.word.word:
-            print("Correct!")
+            print("You got it! Amazing")
+            return True
         else:
-            print("Incorrect!")
+            print("Wrong guess! Try again: ")
+            return False
+    
+    def play(self):
+        print("Welcome to Word Guess! You have " + str(self.guesses)  + " turns to guess the word")
+
+        while self.guesses > 0:
+            guess = input("Please enter your first guess:")
+            if (self.handleGuess(guess)) == True:
+                break
+            else:
+                self.guesses -= 1
+        
+        print("You're out of turns, game over!")
+
 
 # Create a word object
 word = Word()
 game = Game(word)
-
-print(game.handleGuess("able"))
+game.play()
